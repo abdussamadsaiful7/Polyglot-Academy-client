f147e8cd2bdfe8d24fb7998f1e44dc9d8ab4bb73
ABDUS SAMAD SAIFUL <samadsaifullah11@gmail.com>
2023-06-08 14:41:56 +0600

    added show 6 instractors card on home page

---
+++ src/Components/Home/top6Instructor/TopInstructors.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import useInst from '../../../Hook/useInst';
+
+const TopInstructors = () => {
+    const [tops, setTops] = useState([]);
+    const [instructors] = useInst();
+    const popularInst = instructors.filter(item => item.category === "Popular")
+    console.log(popularInst)
+
+
+
+
+    return (
+        <div className='my-20'>
+            <h1 className='text-center font-extrabold text-2xl my-10 uppercase'>Top Instructors in the Polyglot Academy</h1>
+            <div className='grid md:grid-cols-3 gap-4 md:mx-20'>
+                {
+                    popularInst.map(inst =>
+                        <div key={inst._id} className='text-center shadow-lg py-4 shadow-red-400'>
+                            <img className='h-64 w-64 mx-auto' src={inst.image} alt="img" />
+                            <p>Instructor Name: {inst.instructorName}</p>
+                            <p>Email: {inst.email}</p>
+                            <p>Status: {inst.category}</p>
+                            <p>Language: {inst.courses}</p>
+                        </div>
+
+                    )
+                }
+            </div>
+
+        </div>
+    );
+};
+
+export default TopInstructors;
